f70243be7b4cab1fcdef4b08d227f0c97eaac0bf
CPMTooldev <telmnbattr@gmail.com>
2025-02-21 19:34:09 +0800

    officialgoodshit.py

--- officialgoodshit.py
+++ officialgoodshit.py
@@ -1,8 +1,8 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://telmunnshop.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://ashuraa.squareweb.app/api"
 
-class CPMTooldev:
+class OfficialGoodShit:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
