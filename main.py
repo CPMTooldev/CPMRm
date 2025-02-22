bcafc2796e12fcfb0a16f44bb7d2be6b98c5eb3e
CPMTooldev <telmnbattr@gmail.com>
2025-02-22 15:50:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from officialgoodshit import OfficialGoodShit
+from yuxin import CPMTooldev
 
-__CHANNEL_USERNAME__ = "Suprememodzofficiall"
-__GROUP_USERNAME__   = "SuprememodzofficiallChat"
+__CHANNEL_USERNAME__ = "YuxinStore"
+__GROUP_USERNAME__   = "YuxinStoreChat"
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
@@ -134,7 +134,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = OfficialGoodShit(acc_access_key)
+        cpm = CPMTooldev(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
